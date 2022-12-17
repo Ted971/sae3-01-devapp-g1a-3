7a3ff447873c48a66286c684099838fcc4ea281b
Matthis Mabille <96383296+Not-Yukii@users.noreply.github.com>
2022-12-17 02:02:14 +0100

    Ajout Tests BD après création / insertion

---
+++ Documentation/Documentations BD/V2/Tests-BD.sql
@@ -0,0 +1,59 @@
+-----------------------------------------------------------------------------
+--       Tests sur les produits
+-- -----------------------------------------------------------------------------
+
+-- 50 produits differents
+SELECT COUNT(*) FROM Produit;
+
+-- -----------------------------------------------------------------------------
+--       Tests sur les clients
+-- -----------------------------------------------------------------------------
+
+-- 50 clients en France
+SELECT COUNT(*) FROM Client WHERE paysClient = 'France';
+-- Clients répartis sur 35 départements
+SELECT COUNT(UNIQUE departementClient) FROM Client;
+
+-- -----------------------------------------------------------------------------
+--       Tests sur les catégories
+-- -----------------------------------------------------------------------------
+
+-- 35 categorie/sous categorie
+SELECT COUNT(*) as NbrCommande FROM Categorie;
+-- Nombre de sous categorie par categorie
+SELECT C.nomCategorie, COUNT(C2.nomCategorie) as NbrSousCategorie
+FROM Categorie C, Categorie C2 
+WHERE C.idCategorie = C2.idCategoriePere
+GROUP BY C.nomCategorie;
+
+-- -----------------------------------------------------------------------------
+--       Tests sur les commandes
+-- -----------------------------------------------------------------------------
+
+-- 101 commande ont été faites
+SELECT COUNT(*) FROM Commande;
+-- Certains clients n'ont rien commandé
+SELECT COUNT(*) FROM Client
+WHERE idClient NOT IN (SELECT idClient FROM Commande);
+--  Nombre de produits differents par commande
+SELECT C.idCommande, COUNT(UNIQUE P.idProduit) as NbrProduitDiff
+FROM Constituer C, Produit P
+WHERE P.idProduit = C.idProduit
+GROUP BY C.idCommande;
+-- Produits qui sont commandé plusieurs fois dans une commande
+SELECT *
+FROM Constituer
+WHERE qteCommandee>1;
+
+-- -----------------------------------------------------------------------------
+--       Tests sur les paiements
+-- -----------------------------------------------------------------------------
+
+-- Le prix d'une commande peut être calculé
+SELECT C.idCommande, SUM(D.prixProduit*C.qteCommandee) as PrixCommande
+FROM Constituer C, DetailProduit D
+WHERE C.idProduit = D.idProduit
+GROUP BY C.idCommande;
+
+-- Les références de carte bancaire peuvent être conservés
+SELECT * FROM Paiement;
