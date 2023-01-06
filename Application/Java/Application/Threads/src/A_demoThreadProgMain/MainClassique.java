0a98be7340685bc4957b45e86920443a083faa01
Yukii <96383296+Not-Yukii@users.noreply.github.com>
2023-01-06 23:06:00 +0100

    Ajout Application Java
    En cours de réalisation, ceci est la première version

---
+++ Application/Java/Application/Threads/src/A_demoThreadProgMain/MainClassique.java
@@ -0,0 +1,12 @@
+package A_demoThreadProgMain;
+
+public class MainClassique {
+
+	public static void main(String[] args) {
+
+		System.out.println("Main Classique");
+		System.out.println("Ligne 2");
+		System.out.println("Ligne 3");
+
+	}
+}
